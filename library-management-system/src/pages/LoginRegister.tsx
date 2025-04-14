2e55246a941320f30ef578e735c92ddf1fe2666b
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-14 22:08:43 +0800

    started developing scroll mechanism

--- library-management-system/src/pages/LoginRegister.tsx
+++ library-management-system/src/pages/LoginRegister.tsx
@@ -1,16 +1,16 @@
 import { ReactNode } from "react";
 import Background from "../assets/icons/login_background.png";
-// import Login from "../components/login-register/Login";
-// import Register from "../components/login-register/Register";
-// import ForgotPassword from "../components/login-register/ForgotPassword";
-// import OneTimePin from "../components/login-register/OneTimePin";
+import Login from "../components/login-register/Login";
+import Register from "../components/login-register/Register";
+import ForgotPassword from "../components/login-register/ForgotPassword";
+import OneTimePin from "../components/login-register/OneTimePin";
 import ChangePassword from "../components/login-register/ChangePassword";
 
 export default function LoginRegister(): ReactNode {
   return (
     <div
       id="login-register"
-      className="w-screen h-screen overflow-hidden bg-login-page"
+      className="w-screen h-screen overflow-hidden bg-login-page relative"
     >
       <img
         src={Background}
@@ -21,10 +21,16 @@ export default function LoginRegister(): ReactNode {
         id="container"
         className="w-[35vw] h-full flex justify-center items-center flex-col absolute right-0"
       >
-        <h1 className="w-full text-center text-white font-kross-neue-grotesk font-extrabold mb-3 text-3xl">
+        <h1 className="w-full text-center text-white font-kross-neue-grotesk font-extrabold mb-[80%] text-3xl">
           MAWMAW LIBRARY
         </h1>
-        <ChangePassword></ChangePassword>
+        <div id="component-container" className="flex flex-row overflow-x-hidden absolute left-0">
+          <Login></Login>
+          <Register></Register>
+          <ForgotPassword></ForgotPassword>
+          <OneTimePin></OneTimePin>
+          <ChangePassword></ChangePassword>
+        </div>
       </div>
     </div>
   );
