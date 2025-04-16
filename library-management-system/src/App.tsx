d5c55f3536c66c793e6c6ffcbd31658804ddf3fa
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-16 08:46:06 +0800

    added routes for account components

--- library-management-system/src/App.tsx
+++ library-management-system/src/App.tsx
@@ -1,13 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import LoginRegister from "./pages/LoginRegister";
 import { AccountProvider } from "./providers/AccountProvider";
-
+import Login from "./components/login-register/Login";
+import Register from "./components/login-register/Register";
+import ForgotPassword from "./components/login-register/ForgotPassword";
+import OneTimePin from "./components/login-register/OneTimePin";
+import ChangePassword from "./components/login-register/ChangePassword";
 function App() {
+  const LoginRegisterWrapper = () => {
+    return (
+      <LoginRegister>
+        <Outlet></Outlet>
+      </LoginRegister>
+    );
+  };
   return (
     <BrowserRouter>
       <AccountProvider>
         <Routes>
-          <Route path="/login" element={<LoginRegister />}></Route>
+          <Route path="/" element={<LoginRegisterWrapper />}>
+            <Route path="/login" element={<Login></Login>}></Route>
+            <Route path="/register" element={<Register></Register>}></Route>
+            <Route
+              path="/forgot-password"
+              element={<ForgotPassword></ForgotPassword>}
+            ></Route>
+            <Route path="/otp" element={<OneTimePin></OneTimePin>}></Route>
+            <Route
+              path="/change-password"
+              element={<ChangePassword></ChangePassword>}
+            ></Route>
+          </Route>
         </Routes>
       </AccountProvider>
     </BrowserRouter>
